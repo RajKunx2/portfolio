cd6d2f3453ff47a54793b879eb5fdf4ca9cf6bba
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-22 21:43:05 +0530

    3d hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,58 @@
-import React from "react";
+// "use client"
+// import React, { useState, useEffect } from "react";
+// import Loading from "../components/loading";
+// import Hero from "../components/hero";
+
+// const Home = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setIsLoading(false);
+//     }, 10000);
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <>
+//       {isLoading ? (
+//         <Loading />
+//       ) : (
+//         <div className="h-screen flex items-center">
+//           <Hero />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Home;
+
+"use client"
+import React, { useState, useEffect } from "react";
 import Loading from "../components/loading";
+import Hero from "../components/hero";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 100);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Loading />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="h-screen flex items-center">
+          {/* Render the Hero component but keep it hidden */}
+          <Hero className="hidden" />
+        </div>
+      )}
     </>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+import { Application } from '@splinetool/runtime';
+
+const Scene3D = () => {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        // Get a reference to the canvas element
+        const canvas = canvasRef.current;
+
+        // Initialize the application
+        const app = new Application(canvas);
+
+        // Load the Spline scene
+        app.load('https://prod.spline.design/tfT7UR6Oj0N2HguX/scene.splinecode');
+
+        // Cleanup: Make sure to destroy the app when the component unmounts
+        return () => {
+            // app.destroy();
+        };
+    }, []);
+
+    return (
+        <div className="">
+            <canvas ref={canvasRef} className="my-12" id="canvas3d" />
+        </div>
+    );
+};
+
+export default Scene3D;
