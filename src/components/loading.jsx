38f0392f150de6d2c14797ad2f93bfe000cbb382
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-22 16:11:09 +0530

    intro done

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,38 +1,48 @@
-import React from "react";
-import lg from "../assets/lg.gif";
-import md from "../assets/md.gif";
-import sm from "../assets/sm.gif";
+"use client"
+import React, { useState, useEffect } from 'react';
 import Image from "next/image";
+import Lg from "../assets/lg.gif";
+import Md from "../assets/md.gif";
+import Sm from "../assets/sm.gif";
 
-const Loading = () => {
-    // Function to get the current screen size
-    const getScreenSize = () => {
-        if (typeof window !== "undefined") {
-            if (window.innerWidth < 640) return "sm"; // Small screens
-            else if (window.innerWidth < 768) return "md"; // Medium screens
-        }
-        return "lg"; // Default for larger screens or when window is not available
-    };
+const BackgroundChanger = () => {
+    const [screenSize, setScreenSize] = useState('');
 
-    // Get the appropriate image source based on the screen size
-    const getImageSource = () => {
-        const screenSize = getScreenSize();
-        switch (screenSize) {
-            case "sm":
-                return sm;
-            case "md":
-                return md;
-            default:
-                return lg;
+    useEffect(() => {
+        function handleResize() {
+            const screenWidth = window.innerWidth;
+            if (screenWidth < 640) {
+                setScreenSize('small');
+            } else if (screenWidth < 1024) {
+                setScreenSize('medium');
+            } else {
+                setScreenSize('large');
+            }
         }
-    };
+
+        // Initial check
+        handleResize();
+
+        // Event listener to detect screen size changes
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    let imageSource = null;
+
+    if (screenSize === 'small') {
+        imageSource = Sm;
+    } else if (screenSize === 'medium') {
+        imageSource = Md;
+    } else {
+        imageSource = Lg;
+    }
 
     return (
-        <div className="h-screen w-screen">
-            <Image alt="" src={getImageSource()} />
+        <div className={`h-screen flex items-center justify-center ${screenSize === 'small' ? 'bg-screen-small' : ''} ${screenSize === 'medium' ? 'bg-screen-medium' : ''} ${screenSize === 'large' ? 'bg-screen-large' : ''}`}>
+            <Image alt='' src={imageSource} className='object-cover' />
         </div>
     );
 };
 
-export default Loading;
-
+export default BackgroundChanger;
