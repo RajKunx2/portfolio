75ad08b594544bbe755c4d04e17c11a3a0f315c8
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-23 01:03:48 +0530

    model done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import { Application } from '@splinetool/runtime';
+import Spline from '@splinetool/react-spline';
 
-const Scene3D = () => {
-    const canvasRef = useRef(null);
-
-    useEffect(() => {
-        // Get a reference to the canvas element
-        const canvas = canvasRef.current;
-
-        // Initialize the application
-        const app = new Application(canvas);
-
-        // Load the Spline scene
-        app.load('https://prod.spline.design/tfT7UR6Oj0N2HguX/scene.splinecode');
-
-        // Cleanup: Make sure to destroy the app when the component unmounts
-        return () => {
-            // app.destroy();
-        };
-    }, []);
-
-    return (
-        <div className="">
-            <canvas ref={canvasRef} className="my-12" id="canvas3d" />
-        </div>
-    );
-};
-
-export default Scene3D;
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/932DipJhSqid5DcO/scene.splinecode" />
+  );
+}
