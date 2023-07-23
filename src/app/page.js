5f768305b3f04525b6b25f76112e2202d3e80b5e
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-23 17:39:15 +0530

    done the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,55 @@
+// "use client"
+// import React, { useState, useEffect } from "react";
+// import Loading from "../components/loading";
+// import Hero from "../components/Hero";
+// import Navbar from "../components/Navbar";
+// import Socials from "../components/Socials";
+// import About from "../components/About";
+
+// const Home = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setIsLoading(false);
+//     }, 7400);
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <div className="min-h-screen">
+//       {isLoading && (
+//         <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-white">
+//           <Loading />
+//         </div>
+//       )}
+
+//       <div
+//         style={{ visibility: isLoading ? "hidden" : "visible" }}
+//         className="h-screen flex items-center flex-col"
+//       >
+//         {/* <Navbar /> */}
+//         <Hero />
+//       </div>
+//       <div
+//         style={{ visibility: isLoading ? "hidden" : "visible" }}
+//         className="h-screen flex items-center flex-col">
+//         <Socials />
+//         <About />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 "use client"
 import React, { useState, useEffect } from "react";
 import Loading from "../components/loading";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Socials from "../components/Socials";
+import About from "../components/About";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -11,9 +57,15 @@ const Home = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 4);
+    }, 7400);
+
+    // Clean up the timer on unmount
     return () => clearTimeout(timer);
   }, []);
+
+  const handlePageLoad = () => {
+    setIsLoading(false);
+  };
 
   return (
     <div className="min-h-screen">
@@ -34,6 +86,7 @@ const Home = () => {
         style={{ visibility: isLoading ? "hidden" : "visible" }}
         className="h-screen flex items-center flex-col">
         <Socials />
+        <About />
       </div>
     </div>
   );
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const Socials = () => {
   return (
-    <div className='flex flex-col md:flex-row justify-between items-center w-screen'>
-      <button className="button-56 font-inter basis-1/3 md:ml-20" role="button">
-        NEED A INTERN? 🧑‍💻
-      </button>
-      <button className="button-56 font-inter basis-1/3 md:mx-20" role="button">
-       NEED A FREELANCER? 💸
-      </button>
-      <button className="button-56 font-inter basis-1/3 md:mr-20" role="button">
-        CHECK OUT RESUME 👇
-      </button>
-    </div>
+    <>
+      <div className='flex flex-col md:flex-row justify-between items-center w-screen'>
+        <button className="button-56 font-inter basis-1/3 md:ml-20" role="button">
+          NEED A INTERN? 🧑‍💻
+        </button>
+        <button className="button-56 font-inter basis-1/3 md:mx-20" role="button">
+          NEED A FREELANCER? 💸
+        </button>
+        <button className="button-56 font-inter basis-1/3 md:mr-20" role="button">
+          CHECK OUT RESUME 👇
+        </button>
+      </div>
+      <h1 className="text-6xl font-Creative my-16 text-[#E85A4F]">THEN LOOK NO FURTHER!</h1>
+    </>
+
   );
 };
 
