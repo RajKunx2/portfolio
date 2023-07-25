382df01521d54896fed7c9f24d0fe031d63c72a2
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-26 00:48:03 +0530

    summary

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -2,37 +2,37 @@ const Data = [
     {
         img: "/portfolio.png",
         title: "PERSONAL PORTFOLIO",
-        desc: "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural.",
+        desc: "Welcome to my personal portfolio website—a showcase of my diverse talents, creativity, and accomplishments. Let's embark on a journey of innovation and mutual growth.",
         link: "https://portfolio-rajkunx2.vercel.app/",
     },
     {
         img: "/incoming.png",
         title: "INCOMING INDIA",
-        desc: "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural.",
+        desc: "Welcome to Incoming India – your ticket to incredible adventures and cherished memories in the diverse wonders of India. Explore, experience, and embrace the magic of this incredible land with us.",
         link: "https://www.incomingindia.co.in/",
     },
     {
         img: "/toxic.png",
         title: "TOXIC MEDIA",
-        desc: "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural.",
+        desc: "Welcome to Toxic Media – your gateway to captivating content and immersive experiences. Dive into a world of creativity where we craft media that leaves a lasting impact and sparks meaningful connections.",
         link: "https://toxic-media1.vercel.app/",
     },
     {
         img: "/pastry.png",
         title: "INDIAN PASTRY CUP",
-        desc: "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural.",
+        desc: "Indian Pastry Cup- Where top pastry chefs unite, creating a delightful celebration of flavors and artistry. Indulge in an extraordinary culinary journey!",
         link: "https://jrpastryindiancup.vercel.app/",
     },
     {
         img: "/mathsloyal.png",
         title: "MATHSLOYAL",
-        desc: "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural.",
+        desc: "MathsLoyal - Your dedicated support for solving student doubts. Empowering learning through expert guidance and personalized assistance.",
         link: "https://www.mathsloyal.com/",
     },
     {
         img: "/boundless.png",
         title: "BOUNDLESS",
-        desc: "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural.",
+        desc: "Boundless - Empowering lives with dignity. We distribute pads to those in need, fostering a world of equality and compassion.",
         link: "https://hm-boundless.vercel.app/",
     },
 ];
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,57 +1,50 @@
 "use client"
 import React, { useEffect, useState, useRef } from 'react';
-import { motion } from 'framer-motion';
 import Image from "next/image"
-import css from "../assets/css.png"
 import Data from "./Data"
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from "framer-motion";
 
 const ZoomingImage = () => {
-    const wrapRef = useRef(null);
-    const [wrapHeight, setWrapHeight] = useState(200); // Initial wrap height
-    const [scale, setScale] = useState(1); // State to keep track of the scale
 
-    const handleScroll = () => {
-        const vh = window.innerHeight / 100;
-        const scrollTop = document.documentElement.scrollTop;
-        const start = 40 * vh;
-        const stop = wrapHeight * vh;
-
-        if (scrollTop > start && scrollTop < stop) {
-            // Calculate the new scale based on the scroll position
-            const newScale = Math.max(2.2 - (scrollTop - start) / 500, 1);
-            setScale(newScale);
-        } else if (scrollTop <= start) {
-            // Reset the scale to 1 when the user scrolls back to the top
-            setScale(1);
-        }
-    };
+    const controls = useAnimation();
+    const [ref1, inView1] = useInView({ threshold: 0.01 });
 
     useEffect(() => {
-        const updateWrapHeight = () => {
-            // Update wrapHeight state when the window is resized
-            if (wrapRef.current) {
-                setWrapHeight(wrapRef.current.clientHeight);
-            }
-        };
+        if (inView1) {
+            controls.start("visible");
+        }
+    }, [controls, inView1]);
 
-        // Add the scroll event listener when the component mounts
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('resize', updateWrapHeight);
+    const [ref2, inView2] = useInView({ threshold: 0.2 });
 
-        // Remove the scroll event listener when the component unmounts
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-            window.removeEventListener('resize', updateWrapHeight);
-        };
-    }, []);
+    useEffect(() => {
+        if (inView1) {
+            controls.start("visible");
+        }
+    }, [controls, inView2]);
 
     return (
         <div className="flex flex-col items-center h-auto bg-none py-12 w-screen justify-center align-middle rounded-2xl">
-            <motion.section className="wrap rounded-2xl" style={{ height: `${wrapHeight}vh` }} ref={wrapRef} >
-                <motion.h1 style={{ scale: scale }} className="text-center text-5xl lg:text-7xl font-Creative my-8 text-[#d76457]">Projects</motion.h1>
+            <motion.section className="wrap rounded-2xl" >
+                <motion.h1 ref={ref1}
+                    animate={controls}
+                    initial="hidden"
+                    transition={{ duration: 1 }}
+                    variants={{
+                        visible: { opacity: 1, y: 0 },
+                        hidden: { opacity: 0, y: 60 },
+                    }} className="text-center text-5xl lg:text-7xl font-Creative my-8 text-[#d76457]">Projects</motion.h1>
                 <div className='flex flex-col md:flex-row flex-wrap w-screen justify-around rounded-2xl'>
                     {Data.map((item, index) => (
-                        <motion.div style={{ scale: scale }} key={index} className="object-contain mx-4 sm:mx-auto min-mx-6 group relative block bg-black w-auto h-[300px] my-12 rounded-2xl shadow-2xl shadow-[#d76457] border-[#d76457]">
+                        <motion.div ref={ref2}
+                            animate={controls}
+                            initial="hidden"
+                            transition={{ duration: 1 }}
+                            variants={{
+                                visible: { opacity: 1, y: 0 },
+                                hidden: { opacity: 0, y: 60 },
+                            }} key={index} className="object-contain mx-4 sm:mx-auto min-mx-6 group relative block bg-black w-auto h-[300px] my-12 rounded-2xl shadow-2xl shadow-[#d76457] border-[#d76457]">
                             <Image
                                 width={1920}
                                 height={1080}
@@ -60,9 +53,9 @@ const ZoomingImage = () => {
                                 className="absolute inset-0 h-full w-full object-cover opacity-75 rounded-2xl transition-opacity group-hover:opacity-50"
                             />
                             <div className="relative p-4 sm:p-6 lg:p-8 rounded-2xl">
-                                <p className="text-xl font-bold text-white sm:text-2xl">{item.title}</p>
+                                <p className="text-xl font-bold text-white sm:text-2xl font-Dosis">{item.title}</p>
                                 <div className="mt-32 sm:mt-48 lg:mt-64 rounded-2xl">
-                                    <div className="translate-y-8 transform opacity-0 rounded-2xl transition-all ground-hover:-translate-y-8 md:group-hover:-translate-y-32 group-hover:opacity-100">
+                                    <div className="translate-y-8 transform opacity-0 rounded-2xl transition-all ground-hover:-translate-y-32 md:group-hover:-translate-y-32 group-hover:opacity-100">
                                         <p className="text-base mb-2 md:mb-4 md:text-lg md:my-2 font-Josefin font-medium text-white">
                                             {item.desc}
                                         </p>
