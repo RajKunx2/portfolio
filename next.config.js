fd04a0c064aee8b9714d87e7608c587af821db4c
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-25 15:52:24 +0530

    project details left

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {}
 
 module.exports = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com',"i.ibb.co" ],
   },
   theme: {
     extend: {
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -46,23 +46,23 @@ const ZoomingImage = () => {
     }, []);
 
     return (
-        <div className="flex flex-col items-center h-auto bg-[#f4b8af] py-12 w-screen justify-center align-middle">
-            <motion.section className="wrap" style={{ height: `${wrapHeight}vh` }} ref={wrapRef}>
+        <div className="flex flex-col items-center h-auto bg-none py-12 w-screen justify-center align-middle rounded-2xl">
+            <motion.section className="wrap rounded-2xl" style={{ height: `${wrapHeight}vh` }} ref={wrapRef} >
                 <motion.h1 style={{ scale: scale }} className="text-center text-5xl lg:text-7xl font-Creative my-8 text-[#d76457]">Projects</motion.h1>
-                <div className='flex flex-col md:flex-row flex-wrap'>
+                <div className='flex flex-col md:flex-row flex-wrap w-screen justify-around rounded-2xl'>
                     {Data.map((item, index) => (
-                        <motion.div style={{ scale: scale }} key={index} className="object-contain basis-1/2 mx-12 group relative block bg-black w-auto h-[300px] my-6 rounded-lg shadow-2xl shadow-[#d76457] border-[#d76457]">
+                        <motion.div style={{ scale: scale }} key={index} className="object-contain mx-4 sm:mx-auto min-mx-6 group relative block bg-black w-auto h-[300px] my-12 rounded-2xl shadow-2xl shadow-[#d76457] border-[#d76457]">
                             <Image
-                                width={300}
-                                height={200}
+                                width={1920}
+                                height={1080}
                                 alt="Developer"
                                 src={item.img}
-                                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+                                className="absolute inset-0 h-full w-full object-cover opacity-75 rounded-2xl transition-opacity group-hover:opacity-50"
                             />
-                            <div className="relative p-4 sm:p-6 lg:p-8">
+                            <div className="relative p-4 sm:p-6 lg:p-8 rounded-2xl">
                                 <p className="text-xl font-bold text-white sm:text-2xl">{item.title}</p>
-                                <div className="mt-32 sm:mt-48 lg:mt-64">
-                                    <div className="translate-y-8 transform opacity-0 transition-all ground-hover:-translate-y-8 md:group-hover:-translate-y-32 group-hover:opacity-100">
+                                <div className="mt-32 sm:mt-48 lg:mt-64 rounded-2xl">
+                                    <div className="translate-y-8 transform opacity-0 rounded-2xl transition-all ground-hover:-translate-y-8 md:group-hover:-translate-y-32 group-hover:opacity-100">
                                         <p className="text-sm text-white">
                                             {item.desc}
                                         </p>
