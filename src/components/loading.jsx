4ba84c7cac61b82fab48300d9832c9f5a9319ff4
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-08-02 01:41:15 +0530

    loading gif once again

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,8 +1,58 @@
 "use client"
-import React, { useState, useEffect } from 'react';
-import LgVideo from "../assets/lg.mp4";
-import SmVideo from "../assets/sm.mp4";
+// import React, { useState, useEffect } from 'react';
+// import LgVideo from "../assets/lg.mp4";
+// import SmVideo from "../assets/sm.mp4";
 
+// const BackgroundChanger = () => {
+//     const [screenSize, setScreenSize] = useState('');
+
+//     useEffect(() => {
+//         function handleResize() {
+//             const screenWidth = window.innerWidth;
+
+//             if (screenWidth < 768) {
+//                 setScreenSize('small');
+//             } else {
+//                 setScreenSize('large');
+//             }
+//         }
+
+//         // Initial check
+//         handleResize();
+
+//         // Event listener to detect screen size changes
+//         window.addEventListener('resize', handleResize);
+//         return () => window.removeEventListener('resize', handleResize);
+//     }, []);
+
+//     let videoSource = null;
+
+//     if (screenSize === 'small') {
+//         videoSource = SmVideo;
+//     } else {
+//         videoSource = LgVideo;
+//     }
+
+//     return (
+//         <div className={`h-screen flex items-center justify-center ${screenSize === 'small' ? 'bg-screen-small' : ''} ${screenSize === 'large' ? 'bg-screen-large' : ''}`}>
+//             <video autoPlay loop muted className='object-cover'>
+//                 <source src={videoSource} type="video/mp4" />
+//                 Your browser does not support the video tag.
+//             </video>
+//         </div>
+//     );
+// };
+
+// export default BackgroundChanger;
+
+
+
+
+
+import React, { useState, useEffect } from 'react';
+import LgImage from "../assets/lg.gif"; // Replace with the image for large screens
+import SmImage from "../assets/sm.gif"; // Replace with the image for small screens
+import Image from "next/image"
 const BackgroundChanger = () => {
     const [screenSize, setScreenSize] = useState('');
 
@@ -25,20 +75,17 @@ const BackgroundChanger = () => {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-    let videoSource = null;
+    let imageSource = null;
 
     if (screenSize === 'small') {
-        videoSource = SmVideo;
+        imageSource = SmImage;
     } else {
-        videoSource = LgVideo;
+        imageSource = LgImage;
     }
 
     return (
         <div className={`h-screen flex items-center justify-center ${screenSize === 'small' ? 'bg-screen-small' : ''} ${screenSize === 'large' ? 'bg-screen-large' : ''}`}>
-            <video autoPlay loop muted className='object-cover'>
-                <source src={videoSource} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <Image src={imageSource} alt="Background" className='object-cover w-full h-full' />
         </div>
     );
 };
